4fc6ed0b2459b4554e4aef444d583030643c9380
RoshanCD2007 <81854107+RoshanCD2007@users.noreply.github.com>
2021-09-22 17:54:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img = "";
+status_1 = "";
+objects = [];
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+
+    object_detect = ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status_1 = true;
+    object_detect.detect(img , gotResult);
+}
+
+function gotResult(error , results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img , 0 , 0 , 640 , 420);
+    if(status_1 != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            percent = floor(objects[i].confidence * 100);
+            fill("#FF0000");
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
+}
